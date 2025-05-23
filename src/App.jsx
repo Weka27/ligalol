9be2d49210ea9a501abe1a887244a80a76428058
Weka27 <yourdeal24@gmail.com>
2025-05-23 23:37:55 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home.jsx';
-import Register from './pages/Register.jsx';  // falls du schon hast
-// ... andere imports
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
-      {/* Optional Navbar hier */}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        {/* weitere Routen */}
+        {/* Hier kannst du später weitere Routen hinzufügen, z.B. /login, /register */}
       </Routes>
     </Router>
   );
