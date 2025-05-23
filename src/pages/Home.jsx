78cb0b1ea03fae6d11f93a0536389afeab8cae70
Weka27 <yourdeal24@gmail.com>
2025-05-23 23:00:14 +0200

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="p-8">
+      <h1 className="text-4xl font-bold mb-4">Willkommen bei ClassicCore League</h1>
+      <p>Eine Nostalgie Gaming Liga für CS 1.6, Warcraft 3 und mehr.</p>
+    </div>
+  );
+}
