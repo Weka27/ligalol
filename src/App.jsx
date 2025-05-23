df15dadb259957aa5de31b748a7195d841c9fd8e
Weka27 <yourdeal24@gmail.com>
2025-05-23 23:44:39 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+// importiere weitere Seiten hier
 
-function App() {
+export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* Hier kannst du später weitere Routen hinzufügen, z.B. /login, /register */}
+        {/* andere Routes hier */}
       </Routes>
     </Router>
   );
 }
-
-export default App;
