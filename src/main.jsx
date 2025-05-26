ef14f747ffcdf16cc5898d3c6cf818ca5d3fcff5
Weka27 <yourdeal24@gmail.com>
2025-05-26 14:24:30 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import "./styles/index.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
