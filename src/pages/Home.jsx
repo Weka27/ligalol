8c3caaad69861618cdd2888e557514c0457b2a90
Weka27 <yourdeal24@gmail.com>
2025-05-26 14:41:43 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import { AuthContext } from "../context/AuthContext";  // Dein Auth Context
+import { useAuth } from "../AuthContext";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebaseConfig";  // Dein Firebase Setup
 
