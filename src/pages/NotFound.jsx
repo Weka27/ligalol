24ef763efacb0431b0b5ead7faf768f904f2606c
Weka27 <yourdeal24@gmail.com>
2025-05-26 14:01:27 +0200

    Create NotFound.jsx

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+// src/pages/NotFound.jsx
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">404 - Seite nicht gefunden</h1>
+    </div>
+  );
+};
+
+export default NotFound;
