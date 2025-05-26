281ab8b09b8e4565ee7132c593891faf9cf4df2e
Weka27 <yourdeal24@gmail.com>
2025-05-26 13:58:02 +0200

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,21 +1,15 @@
 // src/firebaseConfig.js
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCiU1Eyy6CSnEtFja9UPdZ68Yvw8J1ki8s",
   authDomain: "classicleague-6a087.firebaseapp.com",
   projectId: "classicleague-6a087",
   storageBucket: "classicleague-6a087.firebasestorage.app",
   messagingSenderId: "285835591997",
-  appId: "1:285835591997:web:fdc27da05fd9d362d94554",
-  measurementId: "G-TEBF55MGF2"
+  appId: "1:285835591997:web:fdc27da05fd9d362d94554"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
